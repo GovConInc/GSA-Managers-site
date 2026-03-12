9d39d93ac673cdb6c335954d06301139f00ecc0e
GovConInc <seanconwayemail@gmail.com>
2026-03-11 20:30:40 -0400

    ssc

--- solid-giggle/src/lib/constants.ts
+++ solid-giggle/src/lib/constants.ts
@@ -8,8 +8,12 @@ export const BRAND = {
 };
 
 export const LINKS = {
-  booking: "https://calendar.app.google/EA6JzEhbNTH6AM6S8", // replace with your Google Appointment Schedule / meeting link
+  booking: "https://calendar.app.google/EA6JzEhbNTH6AM6S8",
   linkedin: "#",
+  gsaELibrary: "https://www.gsaelibrary.gsa.gov/",
+  gsaAdvantage: "https://www.gsaadvantage.gov/",
+  fcpPlatform: "https://fcp.gsa.gov/",
+  gsaeBuy: "https://www.ebuy.gsa.gov/",
 };
 
 export const DEFAULT_RSS_URL =
