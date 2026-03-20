71a80b5c6322ff581fda48bcc2e2e64a1a341980
GovConInc <seanconwayemail@gmail.com>
2026-03-19 23:22:02 -0400

    uu

--- solid-giggle/vite.config.ts
+++ solid-giggle/vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     sourcemap: false,
     outDir: "dist",
     emptyOutDir: true,
+    minify: "terser",
     rollupOptions: {
       output: {
         manualChunks: undefined,
